bda6512a9f5486a0400a6a8b41052a4c7f43bf07
phuclh@minerva.vn <hoangphuc98>
2020-10-12 15:40:30 +0700

    --- commit

---
+++ app/src/main/java/com/example/alarmclock/AlarmService.kt
@@ -0,0 +1,11 @@
+package com.example.alarmclock
+
+import android.app.IntentService
+import android.content.Intent
+
+public class AlarmService(name: String) : IntentService(name){
+    override fun onHandleIntent(p0: Intent?) {
+    }
+
+
+}
